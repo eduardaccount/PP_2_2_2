e8322430092805c87ce93624ec37b613a2bef5a8
eduardaccount <100372621+eduardaccount@users.noreply.github.com>
2022-03-10 21:45:28 +0500

    Add files via upload

---
+++ PP_2_2_2_spring-mvc-master/src/web/controller/CarController.java
@@ -0,0 +1,21 @@
+package web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import web.service.CarService;
+
+@Controller
+public class CarController {
+
+    @Autowired
+    private CarService carService;
+
+    @GetMapping(value = "/cars")
+    public String getCountCars(Model model, @RequestParam(defaultValue = "5", value = "count") int number) {
+        model.addAttribute("cars", carService.getCars(number));
+        return "cars";
+    }
+}
