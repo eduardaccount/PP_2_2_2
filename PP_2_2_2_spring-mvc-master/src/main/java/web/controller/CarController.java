37ad104f82ac44558e68d9bdb4854352d5d467e9
eduardaccount <100372621+eduardaccount@users.noreply.github.com>
2022-03-10 21:39:41 +0500

    Add files via upload

--- PP_2_2_2_spring-mvc-master/src/main/java/web/controller/CarController.java
+++ PP_2_2_2_spring-mvc-master/src/main/java/web/controller/CarController.java
@@ -1,10 +1,12 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import web.config.service.CarService;
 import web.model.Car;
 
 import java.util.ArrayList;
@@ -13,28 +15,12 @@ import java.util.List;
 @Controller
 public class CarController {
 
-    static List<Car> repo;
-
-    static {
-        repo = new ArrayList<>();
-        repo.add(new Car("BMW", "X5", "Red"));
-        repo.add(new Car("Audi", "Q5", "Blue"));
-        repo.add(new Car("Lada", "Granta", "Black"));
-        repo.add(new Car("Volvo", "X60", "Yellow"));
-        repo.add(new Car("Citroen", "C5", "White"));
-    }
+    @Autowired
+    private CarService carService;
 
     @GetMapping(value = "/cars")
     public String getCountCars(Model model, @RequestParam(defaultValue = "5", value = "count") int number) {
-        List<Car> currentList = new ArrayList<>();
-        if (number >= 5) {
-            model.addAttribute("cars", repo);
-        } else if (number > 0) {
-            for (int i = 0; i < number; i++) {
-                currentList.add(repo.get(i));
-            }
-            model.addAttribute("cars", currentList);
-        }
+        model.addAttribute("cars", carService.getCars(number));
         return "cars";
     }
 }
